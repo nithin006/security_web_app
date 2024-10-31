a57297c3e382baa1f32f0f2ef126045a41f1eded
nithin <aannanithin@gmail.com>
2024-10-31 11:37:52 +0530

    added security-web-app-files

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header.js';
+import ImageCarousel from './components/ImageCarousel.js';
+import ContentWithImage from './components/ContentWithImage.js';
+import Footer from './components/Footer.js';
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <Header />
+   <ImageCarousel />
+   <ContentWithImage />
+   <Footer />
+   </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+function Header() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">MyWebsite</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="ms-3">Home</Nav.Link>
+            <Nav.Link href="#about" className="ms-3">About</Nav.Link>
+            <Nav.Link href="#services" className="ms-3">Services</Nav.Link>
+            <NavDropdown title="More" id="basic-nav-dropdown" className="ms-3">
+              <NavDropdown.Item href="#contact">Contact</NavDropdown.Item>
+              <NavDropdown.Item href="#blog">Blog</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#faq">FAQ</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
